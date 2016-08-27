fea97291b50dae2146f1f8b360964e137185d66c
qtechbing <sa614226@mail.ustc.edu.cn>
2016-08-27 22:52:21 +0800

    Initial commit

---
+++ secondapp/src/main/java/com/bing/secondapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.bing.secondapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        for(int i = 0; i < 10; i++)
+            System.out.println(i);
+    }
+}
